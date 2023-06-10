2eda28fc3ca9deadaab50bb01a2964dc950c40b1
ankurshashwat <ankurshshwat@gmail.com>
2023-06-10 13:25:10 +0530

    deployed parkade.sol on polygon

--- contract/contracts/Parkade.sol
+++ contract/contracts/Parkade.sol
@@ -7,6 +7,7 @@ contract Parkade {
         uint256 startTime;
         uint256 endTime;
         bool paid;
+        address ownerAddress;
     }
 
     mapping(bytes32 => Reservation) public reservations;
@@ -17,27 +18,36 @@ contract Parkade {
         uint256 startTime,
         uint256 endTime,
         uint256 amount,
-        bool paid
+        bool paid,
+        address ownerAddress
     );
 
     function MakeReservation(
         uint256 _hourlyRate,
         uint256 _startTime,
-        uint256 _endTime
+        uint256 _endTime,
+        address payable _ownerAddress
     ) external payable {
         require(_hourlyRate > 0, "Hourly rate must be greater than zero");
-        require(_endTime > _startTime, "End time must be greater than start time");
+        require(
+            _endTime > _startTime,
+            "End time must be greater than start time"
+        );
+        require(_ownerAddress != address(0), "Invalid owner address");
 
         uint256 duration = _endTime - _startTime;
         uint256 amount = _hourlyRate * duration;
         require(msg.value >= amount, "Insufficient payment");
 
-        bytes32 reservationId = keccak256(abi.encodePacked(msg.sender, block.timestamp));
+        bytes32 reservationId = keccak256(
+            abi.encodePacked(msg.sender, block.timestamp)
+        );
         Reservation memory newReservation = Reservation(
             _hourlyRate,
             _startTime,
             _endTime,
-            true
+            true,
+            _ownerAddress
         );
         reservations[reservationId] = newReservation;
 
@@ -47,11 +57,11 @@ contract Parkade {
             _startTime,
             _endTime,
             amount,
-            true
+            true,
+            _ownerAddress
         );
 
-        address payable contractAddress = payable(address(this));
-        contractAddress.transfer(amount);
+        _ownerAddress.transfer(amount);
     }
 
     receive() external payable {}
