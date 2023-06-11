9ca52970d2aee9fe5c7e14274fec1cecd5a215dd
ankurshashwat <ankurshshwat@gmail.com>
2023-06-11 15:00:29 +0530

    deployed : parkade.sol on polygon network

--- contract/contracts/Parkade.sol
+++ contract/contracts/Parkade.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.9;
 
 contract Parkade {
     struct Reservation {
-        uint256 hourlyRate;
         uint256 startTime;
         uint256 endTime;
         bool paid;
@@ -17,33 +16,24 @@ contract Parkade {
         address indexed user,
         uint256 startTime,
         uint256 endTime,
-        uint256 amount,
         bool paid,
         address ownerAddress
     );
 
-    function MakeReservation(
-        uint256 _hourlyRate,
+    function makeReservation(
         uint256 _startTime,
         uint256 _endTime,
         address payable _ownerAddress
     ) external payable {
-        require(_hourlyRate > 0, "Hourly rate must be greater than zero");
         require(
             _endTime > _startTime,
             "End time must be greater than start time"
         );
-        require(_ownerAddress != address(0), "Invalid owner address");
-
-        uint256 duration = _endTime - _startTime;
-        uint256 amount = _hourlyRate * duration;
-        require(msg.value >= amount, "Insufficient payment");
 
         bytes32 reservationId = keccak256(
             abi.encodePacked(msg.sender, block.timestamp)
         );
         Reservation memory newReservation = Reservation(
-            _hourlyRate,
             _startTime,
             _endTime,
             true,
@@ -56,12 +46,11 @@ contract Parkade {
             msg.sender,
             _startTime,
             _endTime,
-            amount,
             true,
             _ownerAddress
         );
 
-        _ownerAddress.transfer(amount);
+        _ownerAddress.transfer(msg.value);
     }
 
     receive() external payable {}
